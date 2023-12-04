9c1d95b9aace1b135fa45f6ee05b012225452639
Stéphane Payrard <cognominal@gmail.com>
2023-12-05 00:30:28 +0100

    Branch to play with the UI
    want to get a codemirror editor on the left and the ast tree on the right
    
    now got a split pane

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,21 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
+	import { SplitPane } from '@rich_harris/svelte-split-pane';
+  
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+	const dividerColor = 'black';
+	const dividerThickness = '20px';
+  </script>
+  
+  <SplitPane
+	type="horizontal"
+	id="main"
+	min="100px"
+	max="-100px"
+	pos="50%"
+	priority="min"
+	--color={dividerColor}
+	--thickness={dividerThickness}
+  >
+	<section slot="a">this is on the left</section>
+	<section slot="b">this is on the right</section>
+  </SplitPane>
