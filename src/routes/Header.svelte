72f278efb08f273e8a1b32bc38fbb2a0f051dc48
Stéphane Payrard <cognominal@gmail.com>
2023-12-04 01:15:38 +0100

    Added a new page to the rules section

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -19,6 +19,9 @@
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
+			<li aria-current={$page.url.pathname === '/rules' ? 'page' : undefined}>
+				<a href="/rules">Rules</a>
+			</li>
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
 			</li>
